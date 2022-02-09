a656607d64e349fdded136297a32c2ac93b46854
Krešimir Džidić <kresimir.dzidic97@gmail.com>
2022-02-09 17:14:37 +0100

    init

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+
+export default function Button({children, version, type, isDisabled}) {
+  return (
+        <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
+            {children}
+        </button>
+  );
+}
+
+Button.protoTypes = {
+    children: PropTypes.node.isRequired,
+    version: PropTypes.string,
+    type: PropTypes.string,
+    isDisabled: PropTypes.bool
+}
+
+
+Button.defaultProps= {
+    version:'primary',
+    type:'button',
+    isDisabled: false,
+}
