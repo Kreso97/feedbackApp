045ec53843518d4f31d9791d31097655cfcbc11e
Krešimir Džidić <kresimir.dzidic97@gmail.com>
2022-02-13 15:24:11 +0100

    added

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,11 @@
 import FeedbackItem from "./FeedbackItem";
-import PropTypes from "prop-types";
 import { motion, AnimatePresence } from "framer-motion";
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList({  }) {
+  const{feedback} =useContext(FeedbackContext)
+
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet</p>;
   }
@@ -20,7 +23,7 @@ function FeedbackList({ feedback, handleDelete }) {
             <FeedbackItem
               key={item.id}
               item={item}
-              handleDelete={handleDelete}
+              
             />
           </motion.div>
         ))}
@@ -28,22 +31,6 @@ function FeedbackList({ feedback, handleDelete }) {
     </div>
   );
 
-  /*  Bez animacije
-  return (
-    <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
-    </div>
-  );*/
 }
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  ),
-};
 
 export default FeedbackList;
