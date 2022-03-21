c442962217f966e90c8f37eca0298d75f7b09a1e
Krešimir Džidić <kresimir.dzidic97@gmail.com>
2022-03-21 15:10:55 +0100

    Added mock-backend with json server and added spinner component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,19 @@
 import FeedbackItem from "./FeedbackItem";
+import Spinner from "./shared/Spinner";
 import { motion, AnimatePresence } from "framer-motion";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedback yet</p>;
   }
 
-  return (
-    //Animation
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
@@ -28,5 +30,4 @@ function FeedbackList() {
     </div>
   );
 }
-
 export default FeedbackList;
